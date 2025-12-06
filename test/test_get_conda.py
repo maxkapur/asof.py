efa09bffdcdbbf5d5b3531f706fe95fc9f8cfd3d
Max Kapur <git@maxkapur.com>
2025-12-06 08:59:39 -0600

    Add test of get_conda

---
+++ test/test_get_conda.py
@@ -0,0 +1,51 @@
+import datetime
+import re
+
+import pytest
+from packaging.version import Version
+
+from asof.conda import get_conda
+from asof.package_match import PackageMatch
+
+
+@pytest.mark.parametrize(
+    "when,package,expected_matches",
+    [
+        (
+            datetime.datetime.fromisoformat("2022-03-04T00:00:00Z"),
+            "pandas",
+            [
+                PackageMatch(
+                    "pandas",
+                    Version("v1.4.1"),
+                    datetime.datetime.fromisoformat("2022-02-12T06:52:53+00:00"),
+                    "conda-forge",
+                )
+            ],
+        )
+    ],
+)
+def test_get_pypi__ok(
+    when: datetime.datetime, package: str, expected_matches: list[PackageMatch]
+):
+    res = get_conda(when, package)
+    assert res.matches == expected_matches
+    assert res.message is None
+
+
+@pytest.mark.parametrize(
+    "when,package",
+    [
+        (
+            datetime.datetime.fromisoformat("2022-03-04T00:00:00Z"),
+            "DNE_afdgjkfdslghjkdgfhjdkl",
+        )
+    ],
+)
+def test_get_pypi__empty(when: datetime.datetime, package: str):
+    res = get_conda(when, package)
+    assert res.matches == []
+    assert res.message is not None
+    assert re.match(
+        "No matches for .* available from requested conda channels", res.message
+    )
