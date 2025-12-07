def60c04aba302e71491940f1ad1e571ecead224
Max Kapur <git@maxkapur.com>
2025-12-07 07:47:11 -0600

    Remove assertion about pyproject.toml location
    Failed when installing from TestPyPI

--- asof/__init__.py
+++ asof/__init__.py
@@ -7,7 +7,6 @@ from asof.conda import get_conda as get_conda
 from asof.pypi import get_pypi as get_pypi
 
 repo_root = Path(__file__).parent.parent
-assert (repo_root / "pyproject.toml").is_file()
 
 # TODO: Below should be a standalone config file
 pypi_baseurl = "https://pypi.org"
